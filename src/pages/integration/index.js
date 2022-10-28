c50e31b23c11b990e0e224c21f8f86f30effad75
keerthi-mtalkz <keerthi.reddy@mtalkz.com>
2022-10-28 11:17:52 +0530

    added create,add, delete foldres

---
+++ src/pages/integration/index.js
@@ -0,0 +1,149 @@
+import {withRedux} from '../../lib/redux'
+import Layout from '../../layouts'
+import Link from "next/link";
+import { useState } from 'react';
+import Datatable from "../../components/datatable";
+import {ax} from "../../utils/apiCalls";
+import {NotificationManager} from 'react-notifications'
+
+const Integration=()=>{
+ const [integrations,setIntegrations]=useState([])
+ const [status, setStatus] = useState(undefined);
+ const [searchQuery, setSearchQuery] = useState("");
+
+
+  const getIntegrations = async () => {
+    const token = localStorage.getItem('token');
+    await ax
+      .get("/integrations", {headers: {
+        'Authorization': `Bearer ${token}`
+       }})
+      .then((res) => {
+        setIntegrations(res.data);
+      })
+      .catch((err) => {
+        setStatus({ type: "error", err });
+        console.error("get /Integrations error", err);
+      });
+  };
+
+
+  React.useEffect(() => {
+    getIntegrations();
+  }, []);
+
+     const deleteIntegration = (id) => {
+  if (typeof window !== "undefined") {
+  const token = localStorage.getItem('token');
+  const answer = window.confirm("are you sure?");
+  if (answer) {
+    ax.delete(`/integrations/${id}`, {headers: {
+      'Authorization': `Bearer ${token}`
+     }})
+      .then((res) => {
+        setStatus({ type: "success" });
+        setTimeout(() => {
+            getIntegrations();
+        }, 1000);
+      })
+      .catch((err) => {
+        console.error("get /Integrations error", err.message);
+        setStatus({ type: "error", err });
+      });
+  } else {
+    console.log("Thing was not saved to the database.");
+  }
+}
+};
+
+
+  const columns =  [
+    {
+      Header: 'ID',
+      accessor: 'id'
+    },
+      {
+        Header: 'Route',
+        accessor: 'route'
+      },
+      {
+        Header: 'Name',
+        accessor: 'name'
+      },
+      {
+        Header: 'Description',
+        accessor: 'description'
+      },
+      {
+        Header: 'Actions',
+        sortable: false,
+        cell: () => <Button variant="danger" data-tag="allowRowEvents" data-action="delete"><FontAwesomeIcon icon={faTrash} /></Button>,
+        Cell: (data) => {
+       
+        return (<div className="flex justify-evenly"> <Link href={`/integration/view/${data.row.original.id}`}>
+          <p>
+            <i className="icon-eye text-1xl font-bold mb-2"></i>
+          </p>
+      </Link> <p
+        style={{
+         
+          cursor: "pointer",
+          lineHeight: "normal",
+        }}
+        onClick={() => deleteIntegration(data.row.original.id)}><i className="icon-trash text-1xl font-bold mb-2"></i>
+</p>
+<Link href={`/integration/update/${data.row.original.id}`}>
+                      <p>
+                        <i className="icon-note text-1xl font-bold mb-2"></i>
+                      </p>
+                  </Link>
+</div>
+        )}
+       
+      }
+    ]
+  return (
+    <Layout>
+    {status?.type === "success" && (
+      <div className="flex flex-wrap w-full">
+      <div className="p-2">
+      { NotificationManager.success('Deleted integration successfully', 'Success')}
+      </div>
+    </div>
+    )}
+      {status?.type === "error" && (
+        <div className="flex flex-wrap w-full">
+        <div className="p-2">
+        { NotificationManager.error(error, 'Error')}
+         
+        </div>
+      </div>
+      )}
+      <div className="flex flex-row pb-4">
+      <div className=" w-5/6">
+        <input
+          type="text"
+          name="search"
+          className="w-full p-2 ..."
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="Search..."
+        />
+      </div>
+      <div className="w-1/6 ">
+        {" "}
+        <Link href={`/integration/addIntegration`}>
+            <button
+              className="ml-3  btn btn-default btn-indigo create-btn w-full"
+              type="button"
+            >
+              Add Integration
+            </button>
+        </Link>
+      </div>
+    </div>
+    <Datatable columns={columns} data={integrations} />
+    </Layout>
+    )
+}
+
+export default withRedux(Integration)
---
+++ src/pages/integration/update/[updateID].js
@@ -0,0 +1,164 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import Layout from "../../../layouts";
+import SectionTitle from "../../../components/section-title";
+import { useForm } from "react-hook-form";
+import { withRedux } from "../../../lib/redux";
+import { useRouter } from "next/router";
+import {ax} from "../../../utils/apiCalls";
+import {NotificationManager} from 'react-notifications'
+
+
+
+const updateID = () => {
+    const router = useRouter();
+    const updateid = router.query.updateID;
+    const [res, setRes] = useState({});
+    const [status, setStatus] = useState(undefined);
+  
+    const fetch = async () => {
+      if (typeof window !== "undefined") {
+      const token = localStorage.getItem('token');
+      await ax
+        .get(`/integrations/${updateid}`, {headers: {
+        
+          'Authorization': `Bearer ${token}`
+         }})
+        .then((res) => {
+          setRes(res.data.integration);
+
+        })
+        .catch((err) => {
+        setStatus({ type: "error", err });
+
+          console.error("get /integration error", err);
+        });
+      }
+    };
+
+    useEffect(() => {
+      fetch();
+    }, []);
+
+  
+    const { register, handleSubmit, watch, errors } = useForm();
+    const onSubmit = (data) => {
+      if (typeof window !== "undefined") {
+      const token = localStorage.getItem('token');
+      ax.put(`/integrations/${updateid}`, data, {headers: {
+        'Authorization': `Bearer ${token}`
+      }})
+        .then((res) => {
+          setRes(res.data);
+        setStatus({ type: "success" });
+          router.push("/integration");
+        })
+        .catch((err) => {
+          console.error("get /integration error", err);
+          setStatus({ type: "error", err });
+        });
+      }
+    };
+  
+  
+ 
+return (
+    <Layout>
+     <SectionTitle title="UPDATE INTEGRATION" subtitle="" />
+     {status?.type === "success" && (
+        <div className="flex flex-wrap w-full">
+        <div className="p-2">
+        { NotificationManager.success('Updated integration successfully', 'Success')}
+        </div>
+      </div>
+      )}
+      {status?.type === "error" && (
+        <div className="flex flex-wrap w-full">
+        <div className="p-2">
+        { NotificationManager.error(errors, 'Error')}
+        </div>
+      </div>
+      )}
+    
+    
+   
+      <div className="flex flex-wrap ">
+        <div className="w-full">
+        <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col text-sm mb-4 lg:w-1/3"
+    >
+      {/*input*/}
+      <div className="w-full mb-4">
+        <label className="block">
+          <span className="text-default">Route</span>
+          <input
+            name="route"
+            type="text"
+            ref={register({ required: true })}
+            className="form-input mt-1 text-xs block w-full bg-white"
+            placeholder="Route in dot notation"
+            defaultValue={res.route}
+            required
+          />
+        </label>
+        {errors.route && (
+          <p className="mt-1 text-xs text-red-500">{errors.route}</p>
+        )}
+      </div>
+
+      {/*input*/}
+      <div className="w-full mb-4">
+        <label className="block">
+          <span className="text-default">Name</span>
+          <input
+            name="name"
+            type="text"
+            ref={register({ required: true })}
+            className="form-input mt-1 text-xs block w-full bg-white"
+            placeholder="Enter Permission name"
+            defaultValue={res.name}
+            required
+          />
+        </label>
+        {errors.name && (
+          <p className="mt-1 text-xs text-red-500">{errors.name}</p>
+        )}
+      </div>
+
+         {/*input*/}
+         <div className="w-full mb-4">
+         <label className="block">
+           <span className="text-default">Description</span>
+           <input
+             name="description"
+             type="text"
+             ref={register({ required: true })}
+             className="form-input mt-1 text-xs block w-full bg-white"
+             placeholder="Enter Permission description"
+             defaultValue={res.description}
+           />
+         </label>
+         {errors.description && (
+           <p className="mt-1 text-xs text-red-500">{errors.description}</p>
+         )}
+       </div>
+
+
+      {/*input*/}
+
+      <div className="w-full">
+        <input
+          type="submit"
+          className="btn btn-default btn-block btn-indigo btn-rounded"
+          value="Update"
+        />
+      </div>
+    </form>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default withRedux(updateID);
