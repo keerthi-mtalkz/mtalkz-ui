452e300ac4c93b1a4b2c16c4fabe60847fd6d54d
Garuav Agrawal <agragaurav@gmail.com>
2022-12-05 19:16:43 +0530

    Load Voice Bot Builder in iFrame

--- src/pages/voice-campaign/addUpdateCampaign.js
+++ src/pages/voice-campaign/addUpdateCampaign.js
@@ -1,16 +1,22 @@
-import React from 'react'
-import Layout from '../../layouts'
-import {withRedux} from '../../lib/redux'
 import { useRouter } from "next/router";
+import React, { useEffect, useState } from 'react';
+import Layout from '../../layouts';
+import { withRedux } from '../../lib/redux';
 
 const AddUpdateCampaign = () => {
   const router = useRouter();
-  const campaignId = router.query.campaignId;
+  const campaignId = router.query.campaignId || '';
+  const view = router.query.view || '';
+  const [token, setToken] = useState(null)
+
+  useEffect(() => {
+    const t = localStorage.getItem('token');
+    setToken(t);
+  }, [])
+
   return (
     <Layout>
-    <div className="ifbox">
-    <iframe src={campaignId?``:``} title="Create Campaign" id="custom-iframesrc"></iframe>
-    </div>
+      <iframe src={`/voice-bot-builder/index.html?token=${token}&id=${campaignId}&view=${view}`} frameBorder={0} id="custom-iframesrc"></iframe>
     </Layout>
   )
 }
