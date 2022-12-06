93445819470494748c83a50998b27d730684d2a9
keerthi-mtalkz <keerthi.reddy@mtalkz.com>
2022-12-06 16:12:50 +0530

    link chnage

--- src/components/confirmationmodal/index.js
+++ src/components/confirmationmodal/index.js
@@ -13,9 +13,9 @@ const ModalHeader = ({onClick,title}) => (
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         className={`text-secondary stroke-current inline-block h-5 w-5`}>
         <line x1="18" y1="6" x2="6" y2="18"></line>
         <line x1="6" y1="6" x2="18" y2="18"></line>
